67719e08715fe1c5b47192411d2b81e09ade15be
Louis Wu <wulouis@126.com>
2021-09-06 20:50:52 +0800

    First commit

---
+++ PortConnection.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+
+namespace Brokenmai
+{
+    enum FunctionPackage
+    {
+        Rset = 69,
+        Halt = 76,
+        Ratio = 114,
+        Sens = 107,
+        Stat = 65
+    }
+
+    public class PortConnection
+    {
+        private readonly SerialPort _port1;
+        private readonly SerialPort _port2;
+
+        private bool _stage1 = false;
+        private bool _stage2 = false;
+
+        public PortConnection()
+        {
+            // Create ports for both players
+            // DX receives input on COM3(1P) & COM4(2P)
+            _port1 = new SerialPort("COM5", 9600, Parity.None, 8, StopBits.OnePointFive);
+            _port2 = new SerialPort("COM6", 9600, Parity.None, 8, StopBits.OnePointFive);
+            
+
+            _port1.DataReceived += new SerialDataReceivedEventHandler(BytesReceived);
+            _port2.DataReceived += new SerialDataReceivedEventHandler(BytesReceived);
+        }
+
+        public void Start()
+        {
+            _port1.Open();
+            _port2.Open();
+        }
+
+        private void BytesReceived(Object sender, SerialDataReceivedEventArgs args)
+        {
+            SerialPort actualSender = (SerialPort) sender;
+            if (!actualSender.IsOpen) return;
+            int buffer = actualSender.BytesToRead;
+            Byte[] bytes = new byte[buffer];
+            actualSender.Read(bytes, 0, buffer);
+            HandleSerialData(bytes, actualSender);
+        }
+
+        private void HandleSerialData(byte[] data, SerialPort sender)
+        {
+            // Check data[3] for incoming data type
+            switch ((FunctionPackage)data[3])
+            {
+                case FunctionPackage.Rset:
+                case FunctionPackage.Halt:
+                    if (_stage1) return;
+                    Console.WriteLine("Touchscreen startup 1/2");
+                    _stage1 = !_stage1;
+                    return;
+                
+                case FunctionPackage.Ratio:
+                case FunctionPackage.Sens:
+                    byte[] msg = {40, data[1], data[2], data[3], data[4], 41};
+                    sender.Write(msg, 0, msg.Length);
+                    if (_stage2) return;
+                    Console.WriteLine("Touchscreen startup 2/2");
+                    _stage2 = !_stage2;
+                    return;
+
+                case FunctionPackage.Stat:
+                    // Check complete
+                    return;
+            }
+        }
+
+        public void Send(String packet, int player)
+        {
+            SerialPort sender = player == 1 ? _port1 : _port2;
+            List<int> touchInfo = new List<int>();
+            for (int i = 1; i <= 7; i++)
+            {
+                touchInfo.Add(Convert.ToInt32(packet.Substring(5 * i - 5, 5), 2));
+            }
+
+            byte[] bytes =
+            {
+                40,
+                (byte) touchInfo[0],
+                (byte) touchInfo[1],
+                (byte) touchInfo[2],
+                (byte) touchInfo[3],
+                (byte) touchInfo[4],
+                (byte) touchInfo[5],
+                (byte) touchInfo[6],
+                41
+            };
+            
+            sender.Write(bytes, 0, bytes.Length);
+            // Console.WriteLine("Touch sent.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using Brokenmai;
+using iMobileDevice;
+using iMobileDevice.iDevice;
+using iMobileDevice.Lockdown;
+
+namespace Brokenmai_win
+{
+    internal class Program
+    {
+        
+        static Dictionary<string, iDeviceHandle> _devices = new Dictionary<string, iDeviceHandle>();
+
+        private static Dictionary<string, iDeviceConnectionHandle> _connections =
+            new Dictionary<string, iDeviceConnectionHandle>();
+        static IiDeviceApi _idevice = LibiMobileDevice.Instance.iDevice;
+
+        private static bool _exiting = false;
+
+        private static PortConnection _port = new PortConnection();
+
+        public static void Main(string[] args)
+        {
+            NativeLibraries.Load();
+            
+            var status = _idevice.idevice_event_subscribe(EventCallback, IntPtr.Zero);
+            Console.WriteLine("Waiting for device...\nPress [Q] to quit");
+            _port.Start();
+            while (Console.ReadKey().Key != ConsoleKey.Q) { }
+
+            _exiting = true;
+        }
+        
+        private static void EventCallback(ref iDeviceEvent e, IntPtr data)
+        {
+            if (_exiting)
+            {
+                Thread.CurrentThread.Abort();
+            }
+            
+            string udid = e.udidString;
+            iDeviceError status;
+            switch (e.@event)
+            {
+                case iDeviceEventType.DeviceAdd:
+                {
+                    Console.WriteLine("Device detected: {0}", udid);
+                    if (_devices.ContainsKey(udid))
+                    {
+                        return;
+                    }
+
+                    iDeviceHandle handle;
+                    status = _idevice.idevice_new(out handle, udid);
+                    if (status != iDeviceError.Success)
+                    {
+                        Console.WriteLine("Connection failed: {0}", status);
+                        return;
+                    }
+
+                    _devices[udid] = handle;
+
+                    Thread thread = new Thread(Connect);
+                    thread.Start(udid);
+                    break;
+                }
+                case iDeviceEventType.DeviceRemove:
+                {
+                    if (_devices.ContainsKey(udid))
+                    {
+                        iDeviceHandle handle = _devices[udid];
+                        handle.Dispose();
+                        _devices.Remove(udid);
+                    }
+                    break;
+                }
+                case iDeviceEventType.DevicePaired:
+                    Console.WriteLine("Device paired: {0}", udid);
+                    break;
+            }
+        }
+
+        private static void Connect(object arg)
+        {
+            string udid = (string) arg;
+            if (!_devices.ContainsKey(udid))
+            {
+                return;
+            }
+
+            if (_exiting)
+            {
+                Thread.CurrentThread.Abort();
+            }
+
+            iDeviceHandle handle = _devices[udid];
+
+            iDeviceConnectionHandle connHandle = iDeviceConnectionHandle.Zero;
+            iDeviceError status;
+            status = _idevice.idevice_connect(handle, 24864, out connHandle);
+            if (status != iDeviceError.Success)
+            {
+                // Console.WriteLine("Waiting on device...");
+                
+                Thread.Sleep(1000);
+                Thread thread = new Thread(Connect);
+                thread.Start(udid);
+                return;
+            }
+            
+            Console.WriteLine("Device connected.");
+            
+            // Thread.Sleep(200);
+
+            var data = new byte[256];
+            uint read = 0;
+            status = _idevice.idevice_connection_receive(connHandle, data, 14, ref read);
+            if (status != iDeviceError.Success)
+            {
+                Console.WriteLine("Failed receiving data: {0}", status);
+                _exiting = true;
+                return;
+            }
+
+            if (
+                data[0] != 'W' ||
+                data[1] != 'E' ||
+                data[2] != 'L' ||
+                data[3] != 'C'
+                )
+            {
+                Console.WriteLine("Invalid data from {0}", udid);
+                connHandle.Dispose();
+                _exiting = true;
+                Thread.CurrentThread.Abort();
+            }
+            Console.WriteLine("Successfully connected to device {0}", udid);
+            _connections[udid] = connHandle;
+
+            {
+                Thread thread = new Thread(ReadFromDevice);
+                thread.Start(udid);
+            }
+
+            //connHandle.Dispose();
+        }
+
+        public static void ReadFromDevice(object arg)
+        {
+            string udid = (string) arg;
+            if (!_devices.ContainsKey(udid))
+            {
+                return;
+            }
+
+            iDeviceConnectionHandle connHandle = _connections[udid];
+            iDeviceError status;
+
+            byte[] data = new byte[256];
+            uint read = 0;
+            while (true)
+            {
+                if (_exiting) break;
+                status = _idevice.idevice_connection_receive_timeout(connHandle, data, 36, ref read, 5);
+                if (status != iDeviceError.Success)
+                {
+                    if (status == iDeviceError.Timeout)
+                    {
+                        continue;
+                    }
+                    break;
+                }
+
+                if (data[0] != 'I')
+                {
+                    // Invalid header
+                    Console.WriteLine("Invalid header!!");
+                    break;
+                }
+                // Send package to com
+                var sub = new byte[35];
+                for (int i = 0; i <= 34; i++)
+                {
+                    sub[i] = data[i + 1];
+                }
+                // Console.WriteLine(sub.ToString());
+                Console.WriteLine(Encoding.ASCII.GetString(sub));
+                _port.Send(Encoding.ASCII.GetString(sub), 1);
+            }
+            connHandle.Dispose();
+            _connections.Remove(udid);
+            Console.WriteLine("Disconnected from device.");
+            if (_exiting) return;
+            Thread.Sleep(1000);
+            {
+                Thread thread = new Thread(Connect);
+                thread.Start(udid);
+            }
+        }
+    }
+}
